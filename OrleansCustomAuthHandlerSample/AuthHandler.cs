44a1350ee4cf452b9770dd685540975359661bc2
Clayton Lautier <clayton.lautier@cerberus.io>
2017-10-19 17:27:14 +0200

    Initial Commit

---
+++ OrleansCustomAuthHandlerSample/AuthHandler.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace OrleansCustomAuthHandlerSample
+{
+	public class AuthHandler : AuthenticationHandler<JwtBearerOptions>
+	{
+		public AuthHandler(IOptionsMonitor<JwtBearerOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock) : base(options, logger, encoder, clock)
+		{
+		}
+
+		protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
